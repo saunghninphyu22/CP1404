08fe478a42b4b323cc550b19d6fdcaed1f8c5683
saung <saunggg22@gmail.com>
2025-02-11 23:45:00 +0800

    Commit

---
+++ week03/dothisnow01.py
@@ -0,0 +1,40 @@
+# Vd 1
+
+FILENAME = "secret.txt"
+
+def main():
+    secret = load_number(FILENAME)
+    guess = get_valid_number()
+    while guess != secret:
+        print("Guess again")
+        guess = get_valid_number()
+    print("You got it!")
+
+def get_valid_number():
+    """Get a valid number from the user"""
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            guess = int(input("Guess: "))
+            is_valid_input = True
+        except ValueError:
+            print("Invalid integer.")
+    return guess
+
+def load_number(filename):
+    """Load an integer from a file."""
+    try:
+        infile = open(FILENAME, "r")
+        number = int(infile.read())
+    except ValueError:
+        print("Invalid contents in {filename}")
+        number = 6
+    except FileNotFoundError:
+        print("File not found.")
+        number = 4
+    else:
+        infile.close()
+    return number
+
+main()
+
