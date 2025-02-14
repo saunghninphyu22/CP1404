c2d4bb51d63fe350bb7780e6cec1818eeb7d643a
saung <your.email@example.com>
2025-02-14 16:45:58 +0800

    Commit

--- week03/dothisnow02.py
+++ week03/dothisnow02.py
@@ -1,7 +1,15 @@
-infile = open("text.txt")
-for line in infile:
-    if line.startswith("#"):
-        print(line.strip())
-infile.close()
+# infile = open("text.txt")
+# for line in infile:
+#     if line.startswith("#"):
+#         print(line.strip())
+# infile.close()
+
+with open("data.txt") as infile:
+    infile.readline() #ignore header
+    for line in infile:
+        parts = line.strip().split(',')
+        name = parts[0]
+        age = int(parts[1])
+        print(name, "is", age, "years old")
 
 
--- week03/inclass.py
+++ week03/inclass.py
@@ -1,5 +1,19 @@
-infile = open("text.txt")
-for line in infile:
-    if line.startswith("#"):
-        print(line.strip())
-infile.close()
+# infile = open("text.txt")
+# for line in infile:
+#     if line.startswith("#"):
+#         print(line.strip())
+# infile.close()
+from tkinter.font import names
+
+# s = "\tPython, Monty \n"
+# print(s[1],".",sep="")
+# print(s.strip(),".",sep="")
+# s.replace(' ', '*')
+# print(s.lstrip(), ".", sep="")
+# print(s.strip().split(','))
+
+name = input("Enter your name: ")
+outfile = open("name.txt","w")
+print(name,file = outfile)
+outfile.close()
+print("Done")
